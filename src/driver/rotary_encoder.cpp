0f7a335f2eabe94c4b218ef00a500f824a2f904a
iseinko <dev@iseinko.com>
2025-12-11 14:10:06 +0700

    feat: enhance rotary encoder functionality with direction tracking and RPM calculation (2 channel)

--- src/driver/rotary_encoder.cpp
+++ src/driver/rotary_encoder.cpp
@@ -4,8 +4,9 @@ rotary_encoder *rotary_encoder::instance = nullptr;
 bool rotary_encoder::begin()
 {
     instance = this;
-    pinMode(ENCODER_PINS, INPUT);
-    attachInterrupt(digitalPinToInterrupt(ENCODER_PINS), rotary_encoder::isrRouter, RISING);
+    pinMode(ENCODER_PIN_A, INPUT);
+    pinMode(ENCODER_PIN_B, INPUT);
+    attachInterrupt(digitalPinToInterrupt(ENCODER_PIN_A), rotary_encoder::isrRouter, RISING);
     return true;
 }
 
@@ -19,7 +20,22 @@ void rotary_encoder::isrRouter()
 
 void rotary_encoder::handlePulse()
 {
-    Serial.println("Pulse detected");
+    // Read channel B to determine direction
+    // When A rises: if B is LOW, rotating forward; if B is HIGH, rotating reverse
+    bool channelB = digitalRead(ENCODER_PIN_B);
+    
+    if (channelB == LOW)
+    {
+        _pulseCount++; // Forward rotation
+        _direction = 1;
+    }
+    else
+    {
+        _pulseCount--; // Reverse rotation
+        _direction = -1;
+    }
+    
+    // Measure timing for RPM calculation
     if (_measureDone)
     {
         _currentTime = micros();
@@ -41,11 +57,26 @@ float rotary_encoder::getRPM()
     if (timeSinceLastPulse > TIMEOUT || _periodBetweenPulses == 0)
     {
         _rpm = 0;
+        _direction = 0;
     }
     else
     {
-        _rpm = (60000000 / PULSES_PER_REVOLUTION) / _periodBetweenPulses;
+        // Calculate RPM and apply direction
+        _rpm = ((60000000.0 / PULSES_PER_REVOLUTION) / _periodBetweenPulses) * _direction;
     }
 
     return _rpm;
+}
+
+int8_t rotary_encoder::getDirection()
+{
+    // Check if encoder has stopped
+    unsigned long timeSinceLastPulse = micros() - _lastPulseTime;
+    
+    if (timeSinceLastPulse > TIMEOUT)
+    {
+        _direction = 0;
+    }
+    
+    return _direction;
 }
--- src/driver/rotary_encoder.h
+++ src/driver/rotary_encoder.h
@@ -3,8 +3,9 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define ENCODER_PINS 2          // Use interrupt-capable pins
-#define PULSES_PER_REVOLUTION 2 // Set how many pulses there are on each revolution.
+#define ENCODER_PIN_A 2          // Channel A - Use interrupt-capable pins
+#define ENCODER_PIN_B 4          // Channel B - Use interrupt-capable pins
+#define PULSES_PER_REVOLUTION 600 // Set how many pulses there are on each revolution.
 #define TIMEOUT 100000           // Timeout in microseconds to consider the encoder stopped.
 
 class rotary_encoder
@@ -12,11 +13,13 @@ class rotary_encoder
 public:
     bool begin();
     float getRPM();
+    int8_t getDirection(); // Returns 1 for forward, -1 for reverse, 0 for stopped
 
 private:
     static void isrRouter();
     void handlePulse();
-    int16_t _rpm = 0;
+    float _rpm = 0;
+    int8_t _direction = 0; // 1 = forward, -1 = reverse, 0 = stopped
 
     bool _measureDone = false;
     static rotary_encoder *instance; // For ISR routing
@@ -24,4 +27,5 @@ private:
     volatile unsigned long _lastPulseTime = 0;
     volatile unsigned long _currentTime = 0;
     volatile unsigned long _periodBetweenPulses = 1;
+    volatile int32_t _pulseCount = 0; // Track encoder position
 };
