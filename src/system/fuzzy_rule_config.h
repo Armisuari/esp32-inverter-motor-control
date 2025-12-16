3161b9468ebf5ed4bab95f405cd4fa7ce19f4294
Arief Nur Misuari <ariefnurmisuari@gmail.com>
2025-12-16 21:33:26 +0700

    feat: update fuzzy sets for encoder and delta error ranges to improve control accuracy

--- src/system/fuzzy_rule_config.h
+++ src/system/fuzzy_rule_config.h
@@ -13,25 +13,25 @@
     }
 
 // Encoder fuzzy sets
-FUZZY_RANGE(encoder_negatif_besar, 0, 0, 0, 25);
-FUZZY_RANGE(encoder_negatif_kecil, 0, 25, 25, 50);
-FUZZY_RANGE(encoder_zero, 25, 50, 50, 75);
-FUZZY_RANGE(encoder_positif_kecil, 50, 75, 75, 100);
-FUZZY_RANGE(encoder_positif_besar, 75, 100, 100, 100);
+FUZZY_RANGE(encoder_negatif_besar, -300, -300, -300, -150);
+FUZZY_RANGE(encoder_negatif_kecil, -300, -150, -150, 0);
+FUZZY_RANGE(encoder_zero, -50, 0, 0, 50);
+FUZZY_RANGE(encoder_positif_kecil, 0, 150, 150, 300);
+FUZZY_RANGE(encoder_positif_besar, 150, 300, 300, 300);
 
 // Delta error fuzzy sets
-FUZZY_RANGE(deltaerror_turun_cepat, -100, -100, -80, -40);
-FUZZY_RANGE(deltaerror_turun_lambat, -80, -40, -40, 0);
-FUZZY_RANGE(deltaerror_stabil, -40, 0, 0, 40);
-FUZZY_RANGE(deltaerror_naik_lambat, 0, 40, 40, 80);
-FUZZY_RANGE(deltaerror_naik_cepat, 40, 80, 100, 100);
+FUZZY_RANGE(deltaerror_turun_cepat, -100, -100, -100, -50);
+FUZZY_RANGE(deltaerror_turun_lambat, -100, -50, -50, 0);
+FUZZY_RANGE(deltaerror_stabil, -10, 0, 0, 10);
+FUZZY_RANGE(deltaerror_naik_lambat, 0, 50, 50, 100);
+FUZZY_RANGE(deltaerror_naik_cepat, 50, 100, 100, 100);
 
 // Frequency (output) fuzzy sets
-FUZZY_RANGE(frequency_kurangi_banyak, 0, 0, 0, 25);
-FUZZY_RANGE(frequency_kurangi_sedikit, 0, 25, 25, 50);
-FUZZY_RANGE(frequency_tidak_berubah, 25, 50, 50, 75);
-FUZZY_RANGE(frequency_tambah_sedikit, 50, 75, 75, 100);
-FUZZY_RANGE(frequency_tambah_banyak, 75, 100, 100, 100);
+FUZZY_RANGE(frequency_kurangi_banyak, -10, -10, -10, -5);
+FUZZY_RANGE(frequency_kurangi_sedikit, -10, -5, -5, 0);
+FUZZY_RANGE(frequency_tidak_berubah, -1, 0, 0, 1);
+FUZZY_RANGE(frequency_tambah_sedikit, 0, 5, 5, 10);
+FUZZY_RANGE(frequency_tambah_banyak, 5, 10, 10, 10);
 
 // Undefine helper to avoid leaking macro into other translation units.
 #undef FUZZY_RANGE
